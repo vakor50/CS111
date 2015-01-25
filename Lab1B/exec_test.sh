ff510ddf62ab7c1fdfac3548047843b321c96ec8
Kalin Khemka <kalin@kalin-cs111.(none)>
2015-01-24 22:21:56 -0800

    test cases changed

--- Lab1B/exec_test.sh
+++ Lab1B/exec_test.sh
@@ -1,17 +1,28 @@
+#Check simple word
 true
 
+#Check colons
 : : :
 
+#Check incorrect file
+jibjab
+
+#Test simple commands
 echo echo1
+
+#Test pipe
 echo echo4 | cat
 
-echo writing to file > f1.txt
-cat f1.txt # filename as argument
-echo writing to file again > f1.txt
-cat < f1.txt # file from stdin
+#Test simple commands with input-output
+echo wrote to file > f1.txt
+cat f1.txt
+echo wrote to file again > f1.txt
+cat < f1.txt
 
+#Test pipe and sequence commands
 echo echo5 | cat ; echo echo6; echo echo7
 
+#Test if statement
 if :
 then
 echo count
@@ -19,6 +30,7 @@ else
 echo no count
 fi
 
+#Test if statement with input-output
 if :
 then
 echo ball > test.txt
@@ -26,22 +38,32 @@ else
 echo no ball > test.txt
 fi
 
+#More if statement tests
 echo hello > in.txt
 if cat; then cat; fi < in.txt
 
+#Check while loops
 touch file
 touch file2
 echo a > file2
 cp file2 file
 while cat file; do rm file; done
 
+#Check until loops
 touch file
 touch file2
 echo b > file2
 cp file2 file
 until cat file; do rm file; done
 
+#Clean all temp files
 rm test.txt
 rm file2
 rm f1.txt
 rm in.txt
+
+#Test exec command
+exec cat dog
+
+#Test what happens after exec
+echo exec_ran
