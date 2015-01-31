2d552dacd254a82fbe4cbcb42c33405fa9661379
kalinkhemka <kalinkhemka@gmail.com>
2015-01-30 22:52:36 -0800

    idk

--- Lab1C/command.h
+++ Lab1C/command.h
@@ -15,6 +15,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <sys/types.h>  //pid_t
+
 #define SEMICOLON_CHAR		';'
 #define PIPE_CHAR			'|'
 #define PAREN_OPEN_CHAR		'('
