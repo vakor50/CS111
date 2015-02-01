c0eb9f91cb293984e5b334c5c92cee98bbb51dab
kalinkhemka <kalinkhemka@gmail.com>
2015-01-31 19:55:45 -0800

    testing6

--- Lab1C/test.sh
+++ Lab1C/test.sh
@@ -4,3 +4,31 @@ echo ASDFSFADFSAFJFFKFJKFDKFAJFDKFJDKFJDFKSDJKFDSKFDJFKDFJKDSJFKDJFKDSFJDSKFJADS
 cat < txt.txt | cat
 
 rm txt.txt
+
+#Check on SEASNET
+sort /usr/share/dict/words > /dev/null
+
+#Check if commands
+echo hello > in.txt
+if cat; then cat; fi < in.txt
+
+rm in.txt
+
+touch file
+touch file2
+echo a > file2
+cp file2 file
+while cat file; do rm file; done
+
+touch file
+touch file2
+echo b > file2
+cp file2 file
+until cat file; do rm file; done
+
+rm file
+
+#subshells
+(echo a)
+(echo a | cat)
+(echo bob) > file3
