61d79564bfe975f4d3b377478d4efec67db95677
vakor50 <virthakor@ucla.edu>
2015-02-27 20:42:18 -0800

    tests

--- lab3/lab3-tester.pl
+++ lab3/lab3-tester.pl
@@ -102,35 +102,35 @@ close FOO;
       '15'
     ],
     
-    # create symbolic link
+    # create a symbolic link
     # 18
-    [ 'echo foo fight > test/foo; ' .
+    [ 'echo this is foo > test/foo; ' .
       'ln -s foo test/sym && ' .
       'diff test/sym test/foo && ' .
       'rm test/sym test/foo',
       ''
     ],
-    
-    # create symbolic link, change file it points to
+
+    # create a symbolic link and change the file it points to
     # 19
-    [ 'echo foo fight > test/foo; ' .
+    [ 'echo this is foo > test/foo; ' .
       'ln -s foo test/sym && ' .
       'rm test/foo && ' .
-      'echo bar fight > test/foo && ' .
+      'echo this is now bar > test/foo && ' .
       'cat test/sym && rm test/sym test/foo' ,
-      'bar fight'
+      'this is now bar'
     ],
 
-    # check symlink being interpreted relative to the working path
+    # check that a symlink is being interpreted relative to the working path
     # 20
-    [ 'echo foo fight > test/foo; ' .
+    [ 'echo this is foo > test/foo; ' .
       'ln -s foo test/sym && ' .
-      'ln -s foomsg.txt test/subdir/sym && ' .
+      'ln -s message.txt test/subdir/sym && ' .
       'diff <( cat test/foo ) test/sym && ' .
-      'diff <( cat test/subdir/foomsg.txt) test/subdir/sym; ' .
+      'diff <( cat test/subdir/message.txt) test/subdir/sym; ' .
       'cd test/subdir; ' .
       'diff <( cat ../foo ) ../sym && ' .
-      'diff <( cat foomsg.txt) sym; ' .
+      'diff <( cat message.txt) sym; ' .
       'cd ../..; ' .
       'rm test/sym test/subdir/sym test/foo',
       ''
@@ -139,20 +139,20 @@ close FOO;
     # check conditional symlnk
     # 21
     [ 'cd test; '.
-      'echo root_node > rt.txt; ' .
-      'echo not_root_node > not_rt.txt; ' .
-      'chmod 777 not_rt.txt; ' .
-      'ln -s root?rt.txt:not_rt.txt amiroot && ' .
+      'echo Root > root.txt; ' .
+      'echo Not root > not_root.txt; ' .
+      'chmod 777 not_root.txt; ' .
+      'ln -s root?root.txt:not_root.txt amiroot && ' .
       'cat amiroot && ' .
       'su user -c "cat amiroot"; ' .
-      'rm rt.txt not_rt.txt amiroot; ' .
+      'rm root.txt not_root.txt amiroot; ' .
       'cd ..',
-      "root is not root"
+      "Root Not root"
     ],
 
     # another check that symlinks work relative to current path
     # 22
-    [ 'echo foo fight > test/foo; ' .
+    [ 'echo this is foo > test/foo; ' .
       'ln -s ../foo test/subdir/sym && ' .
       'diff test/foo test/subdir/sym && ' .
       'rm test/subdir/sym test/foo',
